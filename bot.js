d2b3d1630bd9442e093a66213831997050e5a8f7
laurence702 <akaigbokwelaurence@gmail.com>
2024-09-25 18:33:25 +0100

    final

--- bot.js
+++ bot.js
@@ -1,8 +1,23 @@
-import OpenAI from "openai";
-const openai = new OpenAI();
-const completion = await openai.chat.completions.create({
-    model: "gpt-4o",
-    messages: [
-        {"role": "user", "content": "write a haiku about ai"}
-    ]
+import { OpenAI } from "openai";
+import { config } from "dotenv";
+import readline from "readline";
+
+config();
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAPI_SECRET,
+});
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+rl.prompt();
+rl.on("line", async (input) => {
+  const res = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    messages: [{ role: "user", content: input }],
+  });
+  console.log(res.data.choices[0].message.content);
+  rl.prompt();
 });
